2c88241a0339afdf7ee4c3a9bc88ea985581f2a0
MattMistele <matthew@mistele.com>
2017-07-28 21:37:23 -0700

    Created project. Updated boostrap.css

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeattleAerialImagery.Startup))]
+namespace SeattleAerialImagery
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
